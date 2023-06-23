70fe5c90afe679b898dfa0bc772969c2793e821b
Omar Ghani <og@Omars-MacBook-Pro.local>
2023-06-23 22:36:31 +0100

    first commit

--- property.py
+++ property.py
@@ -2,62 +2,60 @@ import argparse
 import pandas as pd
 import matplotlib.pyplot as plt
 
-class CaseAnalyzer:
+
+class MortgageDataDates:
     def __init__(self, data_file):
         self.data_file = data_file
 
-    def load_data(self):
-        # Load data from CSV file into a Pandas DataFrame
-        df = pd.read_csv(data_task_part_1.csv)
-        return df
+    def filter_data(self, target_month, property_category=None):
+        # The CSV Data is loaded in
+        df = pd.read_csv(self.data_file)
 
-    def filter_data(self, df, month, category):
-        # Filter data based on specified month and property category (if provided)
-        df['APPLICATION_SUBMITTED_DATE'] = pd.to_datetime(df['APPLICATION_SUBMITTED_DATE'])
-        df['COMPLETED_DATE'] = pd.to_datetime(df['COMPLETED_DATE'])
-        df = df[df['APPLICATION_SUBMITTED_DATE'].dt.strftime('%Y-%m') == month]
+        # Perform a filter query on data in order to only get results with desired month and property-category
+        df_filtered = self.filter_cases(df, target_month, property_category)
 
-        if category:
-            df = df[df['PROPERTY_CATEGORY'] == category]
+        # Time difference between completed and submitted
+        df_filtered['TimeToComplete'] = pd.to_datetime(df_filtered['COMPLETED_DATE']) - pd.to_datetime(df_filtered['APPLICATION_SUBMITTED_DATE'])
+        df_filtered['TimeToComplete'] = df_filtered['TimeToComplete'].dt.days // 30  # Difference in months known as X in question
 
-        return df
+        # print(df_filtered['TimeToComplete'])
 
-    def analyze_cases(self, df):
-        # Calculate the difference in months between application_submitted_date and completed_date
-        df['Completion_Months'] = (df['COMPLETED_DATE'] - df['APPLICATION_SUBMITTED_DATE']) / pd.Timedelta(days=30)
-        df['Completion_Months'] = df['Completion_Months'].astype(int)
+        # This then tallies up the X values and displays them in a new DF
+        countMonthMatch = df_filtered['TimeToComplete'].value_counts().sort_index()
 
-        # Count the number of cases for each completion month
-        case_counts = df['Completion_Months'].value_counts().sort_index()
+        # print(countMonthMatch)
 
-        return case_counts
+        # Generate the plot
+        self.generate_plot(countMonthMatch)
 
-    def plot_cases(self, case_counts):
-        # Plot the number of cases vs. completion months
-        case_counts.plot(kind='bar', xlabel='Completion Months', ylabel='Number of Cases')
-        plt.title('Number of Cases vs. Completion Months')
+    def filter_cases(self, df, target_month, property_category):
+        # Filter cases based on target month
+        df_filtered = df[df['APPLICATION_SUBMITTED_DATE'].str.startswith(target_month)]
+
+        # Filter cases based on property category if provided
+        if property_category:
+            df_filtered = df_filtered[df_filtered['PROPERTY_CATEGORY'] == property_category]
+
+        return df_filtered
+
+    def generate_plot(self, countMonthMatch):
+        plt.plot(countMonthMatch.index, countMonthMatch.values, marker='X', linestyle='-', color='b')
+        plt.xlabel('Time to Completion (Months)')
+        plt.ylabel('Number of Cases')
+        plt.title('Number of Completed Cases over Time')
+        plt.grid(True)
         plt.show()
 
-    def run(self, month, category=None):
-        # Load data, filter, analyze, and plot cases
-        df = self.load_data()
-        df_filtered = self.filter_data(df, month, category)
-        case_counts = self.analyze_cases(df_filtered)
-        self.plot_cases(case_counts)
+
+def main():
+    parser = argparse.ArgumentParser(description='This is an application to find the time taken for an application to be completed given a particular month')
+    parser.add_argument('target_month', type=str, help='Format "YYYY-MM"')
+    parser.add_argument('property_category', type=str, help='Property Category - This can be left in order to perform a generic search*')
+    args = parser.parse_args()
+
+    mDDObject = MortgageDataDates('data_task_part_1.csv')
+    mDDObject.filter_data(args.target_month, args.property_category)
 
 
 if __name__ == '__main__':
-    # Create argument parser
-    parser = argparse.ArgumentParser(description='Case Analyzer')
-    parser.add_argument('month', help='Month in the format "YYYY-MM"')
-    parser.add_argument('--category', help='Optional property category')
-
-    # Parse command-line arguments
-    args = parser.parse_args()
-
-    # Specify the data file path
-    data_file = 'path/to/your/data/file.csv'
-
-    # Create an instance of CaseAnalyzer and run the analysis
-    analyzer = CaseAnalyzer(data_file)
-    analyzer.run(args.month, args.category)
+    main()
